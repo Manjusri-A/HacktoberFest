c5fd373b87a323cc62f0a0dc60c411a3b4958f67
Manjusri Arumugam <msri10792@gmail.com>
2024-10-18 17:47:48 +0530

    strings.java

--- Java Fundamentals programs/strings.java
+++ Java Fundamentals programs/strings.java
@@ -1,12 +1,10 @@
-public class strings
-{
-    public static void main(String[] args)
-    {
+public class Strings { // Class name should start with an uppercase letter
+    public static void main(String[] args) {
         String name = "Keith";
         String other = "Keith";
         
+        // Using equals method to compare strings
         String result = name.equals(other) ? "equal" : "They're not!";
-            System.out.println(result);
-        
+        System.out.println(result);
     }
 }
