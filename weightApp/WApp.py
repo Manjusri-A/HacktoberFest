4eabd0dcf5b065f83331d3b0c30ec1b61761073d
Manjusri Arumugam <msri10792@gmail.com>
2024-10-19 04:07:46 +0530

    Update WApp.py

--- weightApp/WApp.py
+++ weightApp/WApp.py
@@ -6,9 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -16,17 +14,24 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        
+        # Line edit for user input
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(240, 200, 211, 41))
         self.lineEdit.setObjectName("lineEdit")
+        
+        # Button that triggers the action
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(300, 260, 101, 31))
         self.pushButton.setObjectName("pushButton")
+        
+        # Label to display output
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(250, 310, 201, 51))
         self.label.setScaledContents(False)
-        self.label.setAlignment(QtCore.Qt.AlignJustify|QtCore.Qt.AlignVCenter)
+        self.label.setAlignment(QtCore.Qt.AlignJustify | QtCore.Qt.AlignVCenter)
         self.label.setObjectName("label")
+        
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -39,12 +44,19 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        # Connect button click event to a function
+        self.pushButton.clicked.connect(self.update_label)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton.setText(_translate("MainWindow", "Submit"))
         self.label.setText(_translate("MainWindow", "Weight"))
 
+    def update_label(self):
+        # Get text from line edit and set it to label
+        input_text = self.lineEdit.text()
+        self.label.setText(f"Weight: {input_text}")
 
 if __name__ == "__main__":
     import sys
